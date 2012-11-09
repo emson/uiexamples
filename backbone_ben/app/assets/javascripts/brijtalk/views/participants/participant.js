c3f73ea8b6a2bbdeb7c3bec8b5f82e252ea67797
Ben Emson <ben@emson.co.uk>
2012-11-09 12:19:26 +0000

    Added missing msisdn id key to participant html

--- backbone_ben/app/assets/javascripts/brijtalk/views/participants/participant.js
+++ backbone_ben/app/assets/javascripts/brijtalk/views/participants/participant.js
@@ -2,15 +2,24 @@ Brijtalk.Views.Participant= Backbone.View.extend({
   tagName: 'li',
   template: JST['brijtalk/templates/participants/participant'],
 
+  events: {
+   'click' : 'clickAction'
+  },
+
   initialize: function() {
     this.model.on('change', this.render, this);
   },
 
   render: function() {
     var attributes = this.model.toJSON();
+    this.$el.removeAttr('id').attr('id', attributes['msisdn']); // TODO format msisdn
     this.$el.toggleClass("vcard status-" + attributes['state']);
     this.$el.html(this.template( attributes ));
     return this;
+  },
+  
+  clickAction: function(e) {
+    console.log(e);
   },
 
 });
