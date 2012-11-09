2fe8c9dfca7fa7a8f7ee2f350d94329214e06256
otaiga <karl.turner5@gmail.com>
2012-11-09 14:36:06 +0000

    participants view

--- backbone_ben/app/assets/javascripts/brijtalk/views/participants/participant.js
+++ backbone_ben/app/assets/javascripts/brijtalk/views/participants/participant.js
@@ -15,21 +15,22 @@ Brijtalk.Views.Participant= Backbone.View.extend({
     this.$el.removeAttr('id').attr('id', attributes['msisdn']); // TODO format msisdn
     this.$el.toggleClass("vcard status-" + attributes['state']);
     this.$el.html(this.template( attributes ));
+    // bonsai animation
+    bonsai.run(this.el, {
+      code: function() {
+        new Bitmap("http://robohash.org/447715202085.jpg?size=150x170&bgset=bg2").on('load', function() {
+          this.addTo(stage);
+        }).animate('1s', {x:10, y:10}, {easing: 'bounceIn', delay: '100ms'});
+      },
+    width: 500,
+    height: 300
+    });
     return this;
   },
   
   clickAction: function(e) {
     console.log(e);
     target = e.currentTarget;
-    // bonsai animation
-    bonsai.run(target, {
-      code: function() {
-        // new Rect(0,-60,100,100)
-        new Bitmap("http://robohash.org/447715202085.jpg?size=150x170&bgset=bg2").on('load', function() {
-          this.addTo(stage);
-        }).animate('1s', {x:10, y:10}, {easing: 'bounceIn', delay: '100ms'});
-      },
-    });
   },
 
 });
